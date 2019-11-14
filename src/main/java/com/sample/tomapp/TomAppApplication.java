fb3056264003a25512a49be5e507d9f095e0c3c4
Aarooran kanthasamy <aaroor121819@gmail.com>
2019-11-14 15:27:36 +0530

    Initial Pro

---
+++ src/main/java/com/sample/tomapp/TomAppApplication.java
@@ -0,0 +1,13 @@
+package com.sample.tomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/tomapp/TomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.tomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
